2bdd87a538aed2352e7eaa6d3bd589f6aa99f22c
auto <auto@rf-10-mb-pro-2.local>
2017-03-13 15:31:34 -0700

    Initial Commit

---
+++ SettingsViewController.swift
@@ -0,0 +1,64 @@
+//
+//  SettingsViewController.swift
+//  Tip Calculator
+//
+//  Created by auto on 3/12/17.
+//  Copyright © 2017 Silpa. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+
+    @IBOutlet weak var defaultTipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        //loadSettingScreenWithValues()
+    }
+    
+    /**
+     loadSettingScreenWithValues will load the values into the Settings part based on what user had previously set
+     **/
+    /**func loadSettingScreenWithValues() {
+        
+        let defaults = UserDefaults.standard
+        
+        //Setting the default tip percentage in Settings Screen as set by user
+        let selectedDefaultTipPosition = defaults.integer(forKey: "defaultTipPercentageKey")
+    
+        defaultTipControl.selectedSegmentIndex = selectedDefaultTipPosition
+    }**/
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+        
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func setDefaultTipPercent(_ sender: AnyObject) {
+        let defaults = UserDefaults.standard
+        defaults.set(defaultTipControl.selectedSegmentIndex, forKey: "defaultTipControlKey")
+            defaults.synchronize()
+        }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = UserDefaults.standard
+        defaultTipControl.selectedSegmentIndex = defaults.integer(forKey: "defaultTipControlKey")
+    }
+
+}
--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -9,17 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        billField.becomeFirstResponder()
+        
+        let defaults = UserDefaults.standard
+        tipControl.selectedSegmentIndex = defaults.integer(forKey: "defaultTipControlKey")
+        
+        // Animation Block
+        self.tipLabel.alpha = 0
+        self.totalLabel.alpha = 0
+        UIView.animate(withDuration: 0.6, animations: {
+            self.tipLabel.alpha = 1
+            self.totalLabel.alpha = 1
+        })
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let billAmount = Double(billField.text!) ?? 0
+        let tipAmount = billAmount * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = billAmount + tipAmount
+        
+        
+        tipLabel.text = String(format: "$%.2f", tipAmount)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
 }
-
